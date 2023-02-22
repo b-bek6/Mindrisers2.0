08a02f19b508f424b2ef911633e890a226af5899
MacBook <macbook@Bibek-M1.local>
2023-02-22 15:47:50 +0545

    react todos

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import shopping from './component/ShoppingList';
+import TodosApi from './component/TodosApi';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <shopping/>
+  <TodosApi/>
 );
