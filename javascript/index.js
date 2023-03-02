94503013eb224e44a3c1e4e2b84c3daa44f2a63a
MacBook <macbook@Bibek-M1.local>
2023-03-02 16:55:07 +0545

    js variable

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+//dry - donto repeat yourself 
+
+/* Variables
+    <Variable_name> = <value>
+*/
+prefix_sentence = 'we have course' //string variable
+course1 = "Mern"
+price =1500  //int variable
+console.log('hello world');
+console.log("we have course mern");
+console.log(prefix_sentence + ' Marketing')
+
+
+// for = "changed" for is a reserved word so can't be used as variable
+boolean = true; 
+console.log(boolean)
+
+
+
+var brand = "samsung"   //till 1015 samma chai ekdam explictly use garne
+let color = "black"     // ecmascript es6 -2015 ma let ra const intrudece vako
+const PI = 3.14         //
+
+
+var dimension; // variable declaration
+dimension = '3d'; // initialization
